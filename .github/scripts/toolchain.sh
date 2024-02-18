8bdae7e87957af86230b1d08cb4a01d1ba64e4ef
lichanghao.orange <lichanghao.orange@bytedance.com>
2024-02-18 18:11:08 +0800

    add 755

--- .github/scripts/toolchain.sh
+++ .github/scripts/toolchain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo apt -y install build-essential libelf-dev pkg-config
 wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
 chmod +x /tmp/llvm.sh
-./tmp/llvm.sh 17
-apt install libapparmor-dev
+/tmp/llvm.sh 17
+sudo apt -y install libapparmor-dev
