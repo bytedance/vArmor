42ca30970ca6d9fed2117681931adc4269a4e1e0
Danny__Wei <schindler36@live.com>
2026-01-26 20:16:23 +0800

    Merge pull request #286 from bytedance/prepare-for-0.9.1
    Prepare for vArmor v0.9.1 release

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -154,13 +154,13 @@ function QuickStart() {
           <div className={styles.quickStartStep}>
             <h3>1. Fetch chart</h3>
             <CodeBlock language="bash">
-              helm pull oci://elkeid-ap-southeast-1.cr.volces.com/varmor/varmor --version 0.9.0
+              helm pull oci://elkeid-ap-southeast-1.cr.volces.com/varmor/varmor --version 0.9.1
             </CodeBlock>
           </div>
           <div className={styles.quickStartStep}>
             <h3>2. Install</h3>
             <CodeBlock language="bash">
-              helm install varmor varmor-0.9.0.tgz --namespace varmor --create-namespace --set image.registry="elkeid-ap-southeast-1.cr.volces.com"
+              helm install varmor varmor-0.9.1.tgz --namespace varmor --create-namespace --set image.registry="elkeid-ap-southeast-1.cr.volces.com"
             </CodeBlock>
           </div>
           <div className={styles.quickStartStep}>
